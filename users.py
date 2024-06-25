4c3530db1377819295063b4a9a9a37c76e4d3a04
Akalyak3 <147300461+Akalyak3@users.noreply.github.com>
2024-06-25 19:47:44 +0530

    users.py

---
+++ users.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, request, jsonify
+from ..models import db, User
+import bcrypt
+
+users = Blueprint('users', __name__)
+
+@users.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+    new_user = User(username=data['username'], email=data['email'], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User registered successfully!"})
+
+@users.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(email=data['email']).first()
+    if user and bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+        return jsonify({"message": "Login successful!"})
+    else:
+        return jsonify({"message": "Invalid credentials"}), 401
